fda67c454864be1656ca8219bd4510d4f7009a81
jarten-mlg <jarten@mlgtranslate.com>
2024-02-23 23:38:17 -0600

    updated support versions

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 0.15"
+  required_version = "~> 1.7.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.12.0"
+      version = "~> 5.3.0"
     }
   }
 }
